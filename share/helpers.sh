aharri/backupper-server

commit 4fddba945aff70b5ba79d1c8fb64753a81e6e9b3

Convert from getopt to getopts.

share/helpers.sh
@@ -60,27 +60,26 @@ show_usage()
 # Parse command line arguments.
 parse_arguments()
 {
-	dry_run=
-	args=$(getopt vhn $*)
-	if [ $? -ne 0 ]; then
-		show_usage
-	fi
-	set -- $args
-	while [ $# -ge 0 ]; do
-		case "$1" in
-				-v)
-					if [ "$debug" = "YES" ]; then set -x; fi; shift;;
-				-h)
-					show_usage; shift;;
-				-n)
-					rsync_opts="$rsync_opts -n"
-					dry_run=yes
-					shift;;
-				--)
-			shift; break;;
+	local arg
+	while getopts vhn arg "$@"; do
+		case "$arg" in
+			-v)
+				if [ "$debug" = "YES" ]; then set -x; fi;;
+			-h)
+				show_usage;;
+			-n)
+				rsync_opts="$rsync_opts -n"
+				dry_run=yes;;
+			*)
+				show_usage;;
 		esac
 	done
 
+	# No non-option arguments are supported.
+	shift $((OPTIND - 1))
+	if [ $# -ne 0 ]; then
+		show_usage
+	fi
 }
 
 # Prevent shutdown before mail is delivered

commit 0f0b7f7158b276690473abbb10188337f75e3568

Fix command line option parsing.
This was broken by 4fddba945aff70b5ba79d1c8fb64753a81e6e9b3.

share/helpers.sh
@@ -63,11 +63,11 @@ parse_arguments()
 	local arg
 	while getopts vhn arg "$@"; do
 		case "$arg" in
-			-v)
+			v)
 				if [ "$debug" = "YES" ]; then set -x; fi;;
-			-h)
+			h)
 				show_usage;;
-			-n)
+			n)
 				rsync_opts="$rsync_opts -n"
 				dry_run=yes;;
 			*)